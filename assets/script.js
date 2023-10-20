8815166fd45eca0ddded368ca746dabcc528b799
Xander Sumpter <sumpteralexanderl@gmail.com>
2023-10-19 21:02:22 -0500

    newJsFunctions

--- assets/script.js
+++ assets/script.js
@@ -10,74 +10,76 @@ var cityInput = document.querySelector("#city")
 
 //Weather API Section
 
-var cityName 
+var cityName = "Rochester" // need to make the cityName respond to user input
 var APIKey = "af36b85d3236ca25f03ced5a81cc6ee6";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
 
 
-function getCurrentWeather (){
+function getCurrentWeather() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
 
-    fetch(queryURL).then (function(response){
+    fetch(queryURL).then(function (response) {
         return response.json();
     })
-    .then(function(data){
-        
-        var cityNameDataEl = document.createElement("h2")
-        cityNameDataEl.textContent = data.name
-        
-        var weatherForecastListEl = document.createElement("ul")
-        var dateEl = document.createElement("li");
-        dateEl.textContent = "Today's Weather:"
-        var iconListEl = document.createElement("li")
-        var iconEl = document.createElement("img");
-        iconEl.src = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png"
-        iconListEl.appendChild(iconEl)
-        var tempEl = document.createElement("li")
-        tempEl.textContent = "Temp: " +data.main.temp +"°F"
-        var windEl = document.createElement("li")
-        windEl.textContent = "Wind: "+ data.wind.speed +" MPH"
-        var humidityEl = document.createElement("li")
-        humidityEl.textContent = "Humidity: "+data.main.humidity + "%"
-        var weatherDescriptionEl = document.createElement("li")
-        weatherDescriptionEl.textContent = "Description: " +data.weather[0].main
-        
-        //Displays the Weather data
-        cityNameDispayEl.appendChild(cityNameDataEl)
-        weatherForecastListEl.appendChild(dateEl)
-        weatherForecastListEl.appendChild(iconListEl)
-        weatherForecastListEl.appendChild(tempEl)
-        weatherForecastListEl.appendChild(windEl)
-        weatherForecastListEl.appendChild(humidityEl)
-        weatherForecastListEl.appendChild(weatherDescriptionEl)
-        weatherContainerEl.appendChild(weatherForecastListEl)
-        
-        // Populates the weatherTermArray
-        var temp=data.main.temp
-        var windSpeed = data.wind.speed
-        var description = data.weather[0].main
-        if (description == "Clouds"){
-            weatherTermArray.push("cloud")
-        }else
-        {weatherTermArray.push(data.weather[0].main)
-        }
-        if (temp >80){
-        weatherTermArray.push("hot")}
-        if (temp <20){
-        weatherTermArray.push("cold")}
-        if (windSpeed >20){
-        weatherTermArray.push("wind")}
-        console.log(weatherTermArray)
-        console.log(data)
+        .then(function (data) {
+            var cityNameDataEl = document.createElement("h2")
+            cityNameDataEl.textContent = data.name
+
+            var weatherForecastListEl = document.createElement("ul")
+            var dateEl = document.createElement("li");
+            dateEl.textContent = "Today's Weather:"
+            var iconListEl = document.createElement("li")
+            var iconEl = document.createElement("img");
+            iconEl.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+            iconListEl.appendChild(iconEl)
+            var tempEl = document.createElement("li")
+            tempEl.textContent = "Temp: " + data.main.temp + "°F"
+            var windEl = document.createElement("li")
+            windEl.textContent = "Wind: " + data.wind.speed + " MPH"
+            var humidityEl = document.createElement("li")
+            humidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+            var weatherDescriptionEl = document.createElement("li")
+            weatherDescriptionEl.textContent = "Description: " + data.weather[0].main
+
+            //Displays the Weather data
+            cityNameDispayEl.appendChild(cityNameDataEl)
+            weatherForecastListEl.appendChild(dateEl)
+            weatherForecastListEl.appendChild(iconListEl)
+            weatherForecastListEl.appendChild(tempEl)
+            weatherForecastListEl.appendChild(windEl)
+            weatherForecastListEl.appendChild(humidityEl)
+            weatherForecastListEl.appendChild(weatherDescriptionEl)
+            weatherContainerEl.appendChild(weatherForecastListEl)
+
+            // Populates the weatherTermArray
+            var temp = data.main.temp
+            var windSpeed = data.wind.speed
+            var description = data.weather[0].main
+            if (description == "Clouds") {
+                weatherTermArray.push("cloud")
+            } else {
+                weatherTermArray.push(data.weather[0].main)
+            }
+            if (temp > 80) {
+                weatherTermArray.push("hot")
+            }
+            if (temp < 20) {
+                weatherTermArray.push("cold")
+            }
+            if (windSpeed > 20) {
+                weatherTermArray.push("wind")
+            }
+            console.log(weatherTermArray)
+            fetchMusicDataForRandom(weatherTermArray)
 
 
-        //getMusicData() // getMusicData() not yet written
-    })
+            //getMusicData() // getMusicData() not yet written
+        })
 }
 //Music API Section
 var weatherTerm = "rain" // weather term to be updated depending on the day's weather
 var methodChoice = "track.search"
-var methodChoiceValue = "&track=" + weatherTerm
+var methodChoiceValue = "&track=" + weatherTermArray
 var queryModifier = methodChoice + methodChoiceValue
 
 var APIKey2 = "7900466c10eb22d039833bef2573b531"
@@ -118,41 +120,95 @@ var weatherTermArray = [] // to replace var weatherTerms
 // }
 const userSongs = [];
 const saveBtn = document.querySelector("#save-btn");
-saveBtn.addEventListener("click", saveUserInput);
-function saveUserInput(){
-    
+//saveBtn.addEventListener("click", function(){
+
+//)};
+function saveUserInput() {
+
     // const faveSong = document.querySelector("#save-data").value;
 
 
 }
 // define getMusic function + use save button to save favorite song in local storage + display saved song + make getMusic function, save locally + globally
 
-  
-    // getWeather(cityName);
-//function to retrieve the API
-function getMusic(weatherterm) {
-    var APIKey2 = "7900466c10eb22d039833bef2573b531"
-    fetch(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${weatherterm}&api_key=${APIKey2}&format=json`).then(response=>{
-        console.log(response["trackmatches"]["track"][0]);
-        //var randomnumber = Math.floor(Math.random()*100)%response.length;
-        //console.log(response[randomnumber])
-        
+
+// getWeather(cityName);
+// getMusic(weatherTerm);
+
+
+// City Name input set as cityName variable and empty the input after entry
+cityNameForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    if (cityInput.value == "") {
+        return
+    }
+    cityName = cityInput.value
+    cityInput.value = ""
+    getCurrentWeather()
+})
+function getRandomTrack(tracks) {
+    if (tracks.length === 0) {
+        return "No Tracks Available";
+    }
+    const randomIndex = Math.floor(Math.random() * tracks.length);
+    return tracks[randomIndex];
+}
+function getRandomWeather() {
+    if (weatherTermArray.length === 0) {
+        return "No Weather Terms Available";
+    }
+    const randomIndex = Math.floor(Math.random() * weatherTermArray.length);
+    return weatherTermArray[randomIndex];
+}
+
+function fetchMusicDataForRandom(weatherTermArray) {
+    const randomWeatherTerm = getRandomWeather();
+    const methodChoice = "track.search";
+    const methodChoiceValue = "&track=" + weatherTermArray[0];
+    const queryModifier = methodChoice + methodChoiceValue;
+
+    const queryURL2 = "http://ws.audioscrobbler.com/2.0/?method=" + queryModifier + "&api_key=" + APIKey2 + "&format=json";
+
+    fetch(queryURL2, {
+        method: "GET",
+        headers: { 'User-Agent': 'Weather DJW' }
     })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("Data", data)
+            const tracks = data.results.trackmatches.track;
+            if (tracks.length > 0) {
+                const randomTrack = getRandomTrack(tracks);
+                console.log('Randomly selected track:', randomTrack.name);
+                console.log('Track URL:', randomTrack.url);
+                displayRandomTrack(randomTrack.name, randomTrack.url);
+                saveTrackToLocalStorage(randomTrack.name, randomTrack.url);
+            } else {
+                console.log('No tracks found.');
+            }
+        })
+
+
+    function displayRandomTrack(trackName, trackURL) {
+        var trackNameEl = document.createElement("p");
+        var trackLink = document.createElement("a");
+
+        trackLink.textContent = "Now Playing: " + trackName;
+        trackLink.href = trackURL;
+        trackLink.target = "_blank"
+
+        trackNameEl.appendChild(trackLink);
+        songContainerEl.appendChild(trackNameEl);
+        console.log(trackName + trackURL);
+    }
 }
 
 
-  // City Name input set as cityName variable and empty the input after entry
-cityNameForm.addEventListener("submit", function (e){
-    e.preventDefault()
-    if (cityInput.value == ""){
-        return
-    }
 
-    cityName = cityInput.value  
-    cityInput.value = ""
-    weatherContainerEl.innerHTML="" // clears out previous weather if new search is made
-    cityNameDispayEl.innerHTML="" // clears out previous city if new city is searched
-    weatherTermArray= [] // clears the weather array for the new search
-    getCurrentWeather()
-})
-
+// Function to save the current track to local storage
+function saveTrackToLocalStorage(trackName, trackURL) {
+    localStorage.setItem("lastSavedTrackName", trackName);
+    localStorage.setItem("lastSavedTrackURL", trackURL);
+}
